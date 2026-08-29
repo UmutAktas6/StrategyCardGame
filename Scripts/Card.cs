ae5bca1a39b4f1fa9b3d96d92072acccfff05c51
Umut Aktaş <113133658+Umut-Aktas@users.noreply.github.com>
2026-08-29 17:38:53 +0300

    Devam

--- Scripts/Card.cs
+++ Scripts/Card.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class Card
+public class Card : MonoBehaviour, IPointerClickHandler
 {
     public string Name;
     public string Description;
-    public Event OnCardPlayed;
-    private void Update()
+    public UnityEvent OnCardPlayed;
+
+    public void OnPointerClick(PointerEventData eventData)
     {
-        switch(Name)
+        switch (gameObject.name)
         {
             case "MoveOne":
+                Debug.Log("MoveOne card played!");
                 break;
             case "MoveTwo":
+                Debug.Log("MoveTwo card played!");
                 break;
             case "MoveThree":
+                Debug.Log("MoveThree card played!");
                 break;
         }
     }
--- Scripts/DenemeSil.cs
+++ Scripts/DenemeSil.cs
@@ -7,7 +7,7 @@ public class DenemeSil : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Card card = new Card();
     }
 
     // Update is called once per frame
