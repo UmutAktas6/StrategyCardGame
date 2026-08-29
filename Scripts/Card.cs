827200a9a5703e24fae81baafbe14083c9f3a0dd
Umut Aktaş <113133658+Umut-Aktas@users.noreply.github.com>
2026-08-29 17:09:47 +0300

    Card Sistemi Devam

--- Scripts/Card.cs
+++ Scripts/Card.cs
@@ -2,8 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Card : MonoBehaviour
+public class Card
 {
     public string Name;
     public string Description;
+    public Event OnCardPlayed;
+    private void Update()
+    {
+        switch(Name)
+        {
+            case "MoveOne":
+                break;
+            case "MoveTwo":
+                break;
+            case "MoveThree":
+                break;
+        }
+    }
 }
