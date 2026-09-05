518e71ea09b9e72ab99ecf24e67abdd398ded0b3
Umut Aktaş <113133658+Umut-Aktas@users.noreply.github.com>
2026-09-05 14:09:18 +0300

    Düzeltme

--- Scripts/HexMap.cs
+++ Scripts/HexMap.cs
@@ -2,37 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-public class HexMap  
+public class HexMap : MonoBehaviour
 {
-    public Dictionary<Vector3Int, Hex> hexes = new Dictionary<Vector3Int, Hex>();
-    public Dictionary<(Nation, Hex), NationHexState> nationHexStates = new Dictionary<(Nation, Hex), NationHexState>();
+    public Dictionary<Vector3Int, Hex> hexMap;
+    public Dictionary<(Nation, Hex), NationHexState> nationHexStates;
 
-    Hex hex1 = new Hex();
-    Hex hex2 = new Hex();
-
-    Nation nation1 = new Nation();
-    Nation nation2 = new Nation();
-
-    HexMap hexMap = new HexMap();
 
     void Start()
     {
-        hexMap.AddHex(hex1);
-        nationHexStates.Add((nation1, hex1) ,new NationHexState(hex1, nation1));
+        hexMap = new Dictionary<Vector3Int, Hex>();
+        nationHexStates = new Dictionary<(Nation, Hex), NationHexState>();
 
+        var hex1 = new Hex();
+        var hex2 = new Hex();
 
-        var nationHexState = GetOrCreateNationHexState(nation1, hex1);
+        var nation1 = new Nation();
+        var nation2 = new Nation();
+
+        hexMap.Add(Vector3Int.zero, hex1);
+        nationHexStates.Add((nation1, hex1), new NationHexState(hex1, nation1));
+        nationHexStates.TryGetValue((nation1, hex1), out NationHexState nationHexState);
         nationHexState.Explore();
-        
-        HexMap hexagon = new HexMap();
-        hexagon.AddHex(hex1);   
-
     }
+
+
 
 
     public void AddHex(Hex hex)
     {
-        hexes.Add(hex.coordinate, hex);
+        hexMap.Add(hex.coordinate, hex);
     }
 
     public NationHexState GetOrCreateNationHexState(Nation nation, Hex hex)
